eb2f0e36e53d8c7d18349423e63e4ef0c7db8633
Swapnil Bokade <sbokade181@gmail.com>
2025-09-03 18:14:20 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./store/Store.js";
 import { Provider } from "react-redux";
 
+
 // ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 var rootElement = ReactDOM.createRoot(document.getElementById("root"));
